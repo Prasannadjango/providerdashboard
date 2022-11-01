6c3813154692105355160e0b5dd2d5d4872ab1f3
Prasannadjango <Prasannakumarprasanna1199@gmail.com>
2022-11-01 17:46:44 +0530

    changes

--- js/sb-admin-2.js
+++ js/sb-admin-2.js
@@ -54,3 +54,25 @@
   });
 
 })(jQuery); // End of use strict
+
+
+
+$("#profileImage").click(function(e) {
+  $("#imageUpload").click();
+});
+
+function fasterPreview( uploader ) {
+  if ( uploader.files && uploader.files[0] ){
+        $('#profileImage').attr('src', 
+           window.URL.createObjectURL(uploader.files[0]) );
+  }
+}
+
+
+
+
+
+
+
+
+
